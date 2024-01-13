64d746e4d1686140faa0e8194358412032c0c054
iq.dev.expert <iq.dev.expert@gmail.com>
2024-01-13 19:53:06 +0100

    add logo to header and footer

--- app/ui/(components)/footer/footer.jsx
+++ app/ui/(components)/footer/footer.jsx
@@ -1,7 +1,10 @@
+import { Logo } from "@/app/ui/(components)/logo";
+
 export function Footer() {
   return (
     <footer>
       <h1>Footer Component</h1>
+      <Logo />
     </footer>
   );
 }
--- app/ui/(components)/header/header.jsx
+++ app/ui/(components)/header/header.jsx
@@ -1,7 +1,10 @@
+import { Logo } from "@/app/ui/(components)/logo";
+
 export function Header() {
   return (
     <header>
       <h1>Header Component</h1>
+      <Logo />
     </header>
   );
 }
